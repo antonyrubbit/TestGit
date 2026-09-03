4a642df8fdb6d87a4dfb06137f7c3425ffeb4a54
antonyrubbit <antonrubin80@gmail.com>
2026-09-03 16:02:54 +0300

    logic game.py
    The game logic was implemented

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ print("Hello World")
 print("This file was created for learning GitHub")
 secret = random.randint(1,100) # init secret number
 attempt = 0 # counter
+while True:
+  try:
+    guess = int(input("Enter a number: "))
+    attempt += 1
+    if guess < secret:
+      print("The guessed number is greater")
+    elif guess > secret:
+      print("The guessed number is lower")
+    else:
+      print("YOU WON")
+      break
+  except VallueError:
+    print("This is not a number")
+
+print("Thaks for playing")
